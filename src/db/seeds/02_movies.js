c00462197b4659d78d98fe9b70e26726839f4433
Wendy Parsons <wsparsons@gmail.com>
2018-08-06 13:56:24 -0700

    created seed for Frozen movie

--- src/db/seeds/02_movies.js
+++ src/db/seeds/02_movies.js
@@ -1,13 +1,39 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+const table = 'movies'
+exports.seed = knex => {
+  return knex(table).insert([{
+      id: 1,
+      imbdID: 'tt2294629',
+      title: 'Frozen',
+      year: 2013,
+      rated: 'PG',
+      genre: 'Animation, Adventure, Comedy',
+      director: 'Chris Buck, Jennifer Lee',
+      plot: 'When the newly-crowned Queen Elsa accidentally uses her power to turn things into ice to curse her home in infinite winter, her sister Anna teams up with a mountain man, his playful reindeer, and a snowman to change the weather condition.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTQ1MjQwMTE5OF5BMl5BanBnXkFtZTgwNjk3MTcyMDE@._V1_SX300.jpg'
+    }
+    {
+      id: ,
+      imdbID: '',
+      title: '',
+      year: ,
+      rated: '',
+      genre: '',
+      director: '',
+      plot: '',
+      poster: ''
+    },
+    {
+      id: ,
+      imdbID: '',
+      title: '',
+      year: ,
+      rated: '',
+      genre: '',
+      director: '',
+      plot: '',
+      poster: ''
+    }
+  ]).then(() => {
+    return knex.raw(`SELECT setval('${table}_id_seq', (SELECT MAX(id) FROM ${table}))`)
+  });
 };
