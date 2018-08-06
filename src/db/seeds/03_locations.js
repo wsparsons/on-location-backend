5907ca1ddee7d734387e47aad830fe2d936bd878
craig <grendel@shmoo.com>
2018-08-06 14:07:25 -0700

    Added seed files for locations and scenes.

--- src/db/seeds/03_locations.js
+++ src/db/seeds/03_locations.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = knex => {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('locations').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('locations').insert([
+        {id: 1, lat: '-71.907955', long: ' -0.134133', address: 'South Pole, Antarctica'},
+        {id: 2, lat: '48.873834', long: '2.295038', address: 'Arch de Triomphe, Paris, France'},
+        {id: 3, lat: '35.6945174', long: '139.7018175', address: 'Godzilla Head, 1 Chome-18 Kabukicho, Shinjuku, Tokyo, Japan'}      
       ]);
     });
 };
--- src/db/seeds/04_scenes.js
+++ src/db/seeds/04_scenes.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = knex => {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('scenes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('scenes').insert([
+        {id: 1, movie_id: '1', location_id: '1', description: 'The whole movie.'},
+        {id: 2, movie_id: '2', location_id: '2', description: 'The motorcycle chase scene'},
+        {id: 3, movie_id: '3', location_id: '3', description: "Right before godzilla destroys Tokyo."}
       ]);
     });
 };
