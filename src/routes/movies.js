12f1416f830385b743b7276bdc2968d1134b4edd
Wendy Parsons <wsparsons@gmail.com>
2018-08-06 15:41:26 -0700

    created routes for movies and users

---
+++ src/routes/movies.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+const ctrl = require('../controllers/movies')
+
+router.get('/', ctrl.getAll)
+router.get('/:movieId', ctrl.getOne)
+
+module.exports = router
