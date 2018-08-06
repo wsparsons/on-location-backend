c3c51a8b33820402396230be0a7d21c72f30fc64
craig <grendel@shmoo.com>
2018-08-06 14:35:12 -0700

    Refactored scene & location seeds

--- src/db/seeds/03_locations.js
+++ src/db/seeds/03_locations.js
@@ -1,13 +1,26 @@
-
+const table = 'locations'
 exports.seed = knex => {
-  // Deletes ALL existing entries
-  return knex('locations').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('locations').insert([
-        {id: 1, lat: '-71.907955', long: ' -0.134133', address: 'South Pole, Antarctica'},
-        {id: 2, lat: '48.873834', long: '2.295038', address: 'Arch de Triomphe, Paris, France'},
-        {id: 3, lat: '35.6945174', long: '139.7018175', address: 'Godzilla Head, 1 Chome-18 Kabukicho, Shinjuku, Tokyo, Japan'}      
-      ]);
-    });
-};
+// Deletes ALL existing entries
+return knex(table).insert([
+  {
+    id: 1,
+    lat: '-71.907955',
+    long: ' -0.134133',
+    address: 'South Pole, Antarctica'
+  },
+  {
+    id: 2,
+    lat: '48.873834',
+    long: '2.295038',
+    address: 'Arch de Triomphe, Paris, France'
+  },
+  {
+    id: 3,
+    lat: '35.6945174',
+    long: '139.7018175',
+    address: 'Godzilla Head, 1 Chome-18 Kabukicho, Shinjuku, Tokyo, Japan'
+  }
+]).then(() => {
+  return knex.raw(`SELECT setval('${table}_id_seq', (SELECT MAX(id) FROM ${table}));`)
+})
+}
